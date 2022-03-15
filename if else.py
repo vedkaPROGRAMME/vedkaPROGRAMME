26e569f9ff47dc6671473c569b3f953a6abe731d
vedkaPROGRAMME <101658811+vedkaPROGRAMME@users.noreply.github.com>
2022-03-15 22:32:45 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,9 @@
+a = int(input("enter your first number:"))
+b = int(input("enter your ssecond number:"))
+c = int(input("enter your third number:"))
+if(a>b and a>b):
+    print(" "+str(a)+" is greater than "+str(c)+" and "+str(b)")
+elif(b>a and b>a):
+    print(" "+str(b)+" is greater than "+str(a)+"and "+str(c)")
+else:
+    print(" "+str(c)+" is greater than "+str(a)+" and "+str(b)")
