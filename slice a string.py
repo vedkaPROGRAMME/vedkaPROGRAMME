f3aa667c3e6e93bb9c0e3eb89ae5782dbeec82f1
vedkaPROGRAMME <101658811+vedkaPROGRAMME@users.noreply.github.com>
2022-03-15 21:57:01 +0530

    Add files via upload

---
+++ slice a string.py
@@ -0,0 +1,19 @@
+# string operations
+str = 'python is easy !!!'
+print(str)
+print(str[0])
+print(str[3:9])
+print(str[4:])
+print(str[-1])
+print(str[:5])
+print(str * 2)
+print(str + "isn't it?")
+
+tup = ('a' , ('bc'),78,1.23)
+tup2 = ('d',78)
+print(tup)
+print(tup[78])
+print(tup[1:3])
+print(tup[2:])
+print(tup*2)
+pritn(tup + tup2)
