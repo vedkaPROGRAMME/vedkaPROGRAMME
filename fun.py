d5612927b384c25cc57596bc04de261469612693
vedkaPROGRAMME <101658811+vedkaPROGRAMME@users.noreply.github.com>
2022-03-29 18:29:41 +0530

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,6 @@
+def multi():
+    result = a*b
+    print("the multiplication of", a, "and" ,b, "is", result)
+a = int(input("enter your first number:"))
+b = int(input("enter your second number"))
+multi()
